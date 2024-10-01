acb4574559cf5577a20422a4df06d7c2ba27b1d6
Nguyễn Thế Việt <vietnguyenthe810@gmail.com>
2024-10-01 12:38:49 +0700

    .

--- src/components/footer_components/SocialLinks.js
+++ src/components/footer_components/SocialLinks.js
@@ -6,7 +6,7 @@ const SocialLinks = () => {
         <div className="w-full md:w-1/3">
             <h3 className="text-xl font-bold mb-3">Follow Us</h3>
             <div className="flex text-white space-x-4">
-                <a href="#" className="p-3 bg-orange-500 rounded-full hover:bg-orange-300">
+                <a href="https://www.facebook.com/profile.php?id=61566557542840&locale=vi_VN" className="p-3 bg-orange-500 rounded-full hover:bg-orange-300">
                     <FaFacebookF />
                 </a>
                 <a href="#" className="p-3 bg-orange-500 rounded-full hover:bg-orange-300">
